6d53b18eef7a2b8e69dad9219be033caeeae2efa
Tomás Costa Barros <up202303664@up.pt>
2026-09-22 11:54:25 +0100

    Add files via upload

---
+++ TP1.hs
@@ -0,0 +1,13 @@
+double x = x * 2
+
+incr :: Int -> Int
+incr x = x+1
+triple :: Int -> Int
+triple x = 3*x
+welcome :: String -> String
+welcome name = "Hello, " ++ name ++ "!"
+count :: String -> String
+count str = show (length str) ++ " characters."
+
+second :: [a] -> a
+second xs = head (tail xs)
